693d0ba3b393eb145efb2ac1e22814b3f64120ce
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-06-06 01:14:44 -0300

    Conclusão do backend

---
+++ backend/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import PointController from './controllers/PointController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const pointController = new PointController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointController.create);
+routes.get('/points', pointController.index);
+routes.get('/points/:id', pointController.show);
+
+export default routes;
---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || '3333';
+import express from 'express';
+import cors from 'cors';
+import path from 'path'
+import { json, urlencoded } from 'body-parser';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+})
